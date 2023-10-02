a5591b043b5808a9107b9d212eea7deb2c6dca8e
Henrik Vester <henrik645@gmail.com>
2023-10-02 12:52:41 +0200

    Update TestExpr to use our InputParser instead
    Co-authored-by: gustafjonson <gustafjonson@users.noreply.github.com>
    Co-authored-by: Victor Pekkari <victor99pekk@users.noreply.github.com>
    Co-authored-by: Joel Bengs <joel.bengs@gmail.com>

--- app/src/main/java/xl/expr/TestExpr.java
+++ app/src/main/java/xl/expr/TestExpr.java
@@ -1,37 +1,38 @@
 package xl.expr;
 
-import java.io.IOException;
+import xl.expr.factories.InputParser;
 
 public class TestExpr {
 
     public static void main(String[] args) {
-        ExprParser parser = new ExprParser();
-        try {
-            Expr expr = parser.build("1+2*3");
-            System.out.println(expr);
-            System.out.println(expr.value(null));
-            expr = parser.build("A3+A2*A1");
-            Environment env =
-                    new Environment() {
-                        @Override
-                        public double value(Coordinate coordinate) {
-                            String name = coordinate.toString();
-                            if (name.equals("A3")) return 1;
-                            if (name.equals("A2")) return 2;
-                            if (name.equals("A1")) return 3;
-                            System.out.println(name + " is undefined");
-                            return 0;
-                        }
+        InputParser parser = new InputParser();
+        Cell expr = parser.parse("1+2*3");
+        System.out.println(expr);
+        System.out.println(expr.value(null));
+        expr = parser.parse("#this is a comment");
 
-                        @Override
-                        public void addToSheet(Coordinate coordinate, Cell cell) {
-                            // do nothing
-                        }
-                    };
-            System.out.println(expr);
-            System.out.println(expr.value(env));
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
+        System.out.println(expr);
+        System.out.println(expr.value(null));
+
+        expr = parser.parse("A3+A2*A1");
+        Environment env =
+                new Environment() {
+                    @Override
+                    public double value(Coordinate coordinate) {
+                        String name = coordinate.toString();
+                        if (name.equals("A3")) return 1;
+                        if (name.equals("A2")) return 2;
+                        if (name.equals("A1")) return 3;
+                        System.out.println(name + " is undefined");
+                        return 0;
+                    }
+
+                    @Override
+                    public void addToSheet(Coordinate coordinate, Cell cell) {
+                        // do nothing
+                    }
+                };
+        System.out.println(expr);
+        System.out.println(expr.value(env));
     }
 }
