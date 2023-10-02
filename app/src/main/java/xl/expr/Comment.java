012c01431305b7f6802eb4b3164b6eba26492704
Henrik Vester <henrik645@gmail.com>
2023-10-02 10:19:13 +0200

    Add Comment

---
+++ app/src/main/java/xl/expr/Comment.java
@@ -0,0 +1,24 @@
+package xl.expr;
+
+public class Comment implements Cell {
+
+    String message;
+
+    public Comment(String message) {
+        this.message = message;
+    }
+
+    public double value(Environment env) {
+        return 0; // comment slots return the value 0
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+
+    @Override
+    public String toString(int prec) {
+        return message;
+    }
+}
